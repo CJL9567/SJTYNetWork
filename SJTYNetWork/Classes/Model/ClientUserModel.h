45e5b11cf3f2a2caab3f43ac8de41bf3bed7dcce
caijialiang <1350027967@qq.com>
2021-09-28 15:33:11 +0800

    1.0.6
    1.0.6 ---->2021-9-28
    添加用户相关缺少字段

--- SJTYNetWork/Classes/Model/ClientUserModel.h
+++ SJTYNetWork/Classes/Model/ClientUserModel.h
@@ -48,6 +48,9 @@ NS_ASSUME_NONNULL_BEGIN
 ///用户名（默认值顺序：设置值、邮箱、电话）
 @property(nonatomic,copy)NSString *username;
 
+///颜值
+@property(nonatomic,copy)NSString *salt;
+
 
 @property(nonatomic,strong)ClientUserInfoModel *clientUserInfo;
 @end
@@ -99,6 +102,29 @@ NS_ASSUME_NONNULL_BEGIN
 ///备注
 @property(nonatomic,copy)NSString *remark;
 
+///紧急联系人
+@property(nonatomic,copy)NSString  *emergencyContact;
+
+///紧急联系人id
+@property(nonatomic,copy)NSString *emergencyContactId;
+
+///紧急联系人电话
+@property(nonatomic,copy)NSString *emergencyContactPhone;
+
+///扩展字段(根据各个项目决定，后续可扩展为es)
+@property(nonatomic,copy)NSString *extJson;
+
+///扩展字段1
+@property(nonatomic,copy)NSString *param1;
+
+///扩展字段2
+@property(nonatomic,copy)NSString *param2;
+
+///扩展字段3
+@property(nonatomic,copy)NSString *param3;
+
+///扩展字段4
+@property(nonatomic,copy)NSString *param4;
 
 
 @end
